0935e7179a8f3025cf3807423ed540707b8aabff
dYoshitakaKato <phoenix1331019@gmail.com>
2019-08-25 02:34:32 +0900

    Add https://futurismo.biz/archives/6087

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -28,5 +28,13 @@ function fish_user_key_bindings
   bind \cx\ck peco_kill
   bind \c] peco_select_ghq_repository
   bind \cx\cr peco_recentd
+  # gh-open
+  bind \cx\cl peco_open_gh_repository
+  # fzf
+  bind \cx\cf '__fzf_find_file'
+  bind \ctr '__fzf_reverse_isearch'
+  bind \ex '__fzf_find_and_execute'
+  bind \ed '__fzf_cd'
+  bind \eD '__fzf_cd_with_hidden'
 end
 
